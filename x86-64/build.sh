a4efb8927b1886a935d73c32b023a47783c570a4
yangyanch <53054221+yangyanch@users.noreply.github.com>
2024-12-31 16:50:05 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -22,6 +22,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-app-msd_lite"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
